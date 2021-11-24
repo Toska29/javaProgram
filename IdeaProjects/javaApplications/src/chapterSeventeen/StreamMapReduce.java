f6322f741aef9ce467d4c380e99c8a7073ba689c
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2021-11-24 11:00:14 +0100

    intermediate Operation

---
+++ IdeaProjects/javaApplications/src/chapterSeventeen/StreamMapReduce.java
@@ -0,0 +1,10 @@
+package chapterSeventeen;
+
+import java.util.stream.IntStream;
+
+public class StreamMapReduce {
+    public static void main(String[] args) {
+        System.out.printf("Sum of the even int from 2 through 20 is: %d%n",
+                IntStream.rangeClosed(1, 10).map((int x) -> {return x*2;}).sum());
+    }
+}
